01559c72a86bc5caac5c8efc4c12c7e08f629224
lnsvr <156704662+lnsvr@users.noreply.github.com>
2024-03-25 17:09:48 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+let books = ["The Hunger Games",
+"The end",
+"How to have a good day",
+"Happy people"];
+console.log(books);
+
+
+let films = ["The Hunger Games",
+"Harry Potter",
+"Home Alone"];
+console.log(films);
+
+
+let allMassives = books.concat(films,);
+allMassives.push('HP ZBook G7 Mobile');
+allMassives.unshift('+380689586991');
+allMassives.unshift('red');
+allMassives.shift();
+console.log(allMassives);
+
